7816bb2eb5ef80e24991edf608d2880c40b9b17b
Sreehari Mohan <sreeharimohan11@gmail.com>
2017-11-14 16:53:00 +0530

    test fixes

--- compare_test.go
+++ compare_test.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
 func TestIsImageTagOutdated(t *testing.T) {
+	os.Setenv("CLEANISTRY_DOCKER_HOST_URL", "dockerhub.myntra.com:5000")
+	os.Setenv("CLEANISTRY_IMAGE_TAG_EXCEMPTION_TEST_API", "dockins.myntra.com/data/getRC")
 	_, err := IsImageTagOutdated("absolut-saloni_vb", "0910506-1ba49d7")
 	FailOnError(err)
 }
